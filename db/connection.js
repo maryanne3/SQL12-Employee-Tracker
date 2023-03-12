703196daea0da28e6383848d24569f97db52c50f
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-03-12 13:39:58 +0000

    database connection.js

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+// Connection to the database
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'employee_DB',
+  
+  },
+  console.log(` Great! You are connected to the employee-tracker database.`)
+);
+
+module.exports = connection
